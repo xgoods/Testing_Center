c391cdfa2cc88fb9a86ba56d2d0f72c55f4744b2
xgoods <goods.brian@gmail.com>
2017-10-20 18:14:48 -0400

    fixed login

--- brian_frontstuff/login.php
+++ brian_frontstuff/login.php
@@ -1,16 +1,14 @@
 <?php
 
-if (isset($_POST['submitButton'])){
-   session_start();
     $username = $_POST["user"];
     $password = $_POST["pass"];
     
     $creds = array('username'=>$username,'password'=>$password);
-    $credsToSend = json_encode($creds);
+    $url = "https://web.njit.edu/~kl297/loginmid.php";
     $mid = curl_init();
-    curl_setopt($mid, CURLOPT_URL, "https://web.njit.edu/~kl297/loginmid.php");
+    curl_setopt($mid, CURLOPT_URL, $url);
     curl_setopt($mid, CURLOPT_POST, 1);
-    curl_setopt($mid, CURLOPT_POSTFIELDS, $credsToSend);   
+    curl_setopt($mid, CURLOPT_POSTFIELDS, $creds);   
     curl_setopt($mid, CURLOPT_FOLLOWLOCATION, 1);
     $midexec = curl_exec($mid);
     curl_close($mid);
