c514f4ab15d5b35c864ba81c6bec5c2f0c2495e6
ad379 <ashd65@gmail.com>
2017-10-22 15:43:52 -0400

    Create GetGradingRubric.php

---
+++ GetGradingRubric.php
@@ -0,0 +1,24 @@
+<?php
+class GetGradingRubric {
+	public function post($data,$db) {
+		$qid = $data['qid'];
+		$ques = mysqli_query($db,"SELECT * FROM Bank WHERE qid = '$qid'");
+		$question = mysqli_fetch_array($result);
+		if ($question = NULL) {
+			die(json_encode(array(
+				"status" => 0,
+				"message" => "question does not exist")));
+    } else {
+			$result = mysqli_query($db,"SELECT fname FROM Bank WHERE qid = '$qid';");
+      $row = mysqli_fetch_array($result);
+      $fname = $row['fname'];
+      $arg1 = $row['arg1'];
+      $arg2 = $row['arg2'];
+      $arg3 = $row['arg3'];
+		die(json_encode(array(
+			"status" => 1
+      "fname" => $fname)));
+   }
+	}
+}
+?>
