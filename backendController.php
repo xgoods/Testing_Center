c680e66b9ed27b3d14bdae1a0f29ba3533d2c191
ad379 <ashd65@gmail.com>
2017-10-22 12:17:21 -0400

    Create backendController.php

---
+++ backendController.php
@@ -0,0 +1,82 @@
+<?php
+/* backendController.php
+ * central backend hub - routes all requests from middle based
+ * on a command field
+ */
+ 
+require 'Login.php';
+require 'CreateQuestion.php';
+require 'GetBank.php';
+require 'CreateExam.php';
+require 'ListExams.php';
+require 'GetExam.php';
+require 'GetGrades.php';
+require 'SetStudentAnswers.php';
+require 'SetStudentGrade.php';
+require 'GetStudentAnswer.php';
+require 'ReleaseExams.php';
+$data = json_decode(file_get_contents('php://input'),true);
+$action;
+if (!isset($data['cmd'])) {
+	http_response_code(501);
+	die(json_encode(array(
+		"message" => "command field not set")));
+}
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
+switch ($data['cmd']) {
+	case "login":
+		$action = new Login;
+		$action->post($data,$db);
+		break;
+	case "createquestion":
+		$action = new CreateQuestion;
+		$action->post($data['data'],$db);
+		break;
+	case "bank":
+		$action = new GetBank;
+		$action->post($db, $data['data']);
+		break;
+	case "createExam":
+		$action = new CreateExam;
+		$action->post($data['data'],$db);
+		break;
+	case "exams":
+		$action = new ListExams;
+		$action->post($data,$db);
+		break;
+	case "getExam":
+		$action = new GetExam;
+		$action->post($data,$db);
+		break;
+	case "getGrades":
+		$action = new GetGrades;
+		$action->post($data,$db);
+		break;
+	case "answered":
+		$action = new SetStudentAnswers;
+		$action->post($data,$db);
+		break;
+	case "setStudentGrade":
+		$action = new SetStudentGrade;
+		$action->post($data,$db);
+		break;
+	case "getStudentAnswer":
+		$action = new GetStudentAnswer;
+		$action->post($db);
+		break;
+    case "release":
+		$action = new ReleaseExams;
+		$action->post($db, $data['data']);
+		break;
+}
+http_response_code(501);
+mysqli_close($db);
+die(json_encode(array(
+	"message" => "invalid command or not yet implemented")));
+?>
