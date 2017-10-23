5d05b77af0f40e789c01bbcaab74a37065f5ba5f
awdeeyo <kwevinle@gmail.com>
2017-10-23 15:42:32 -0400

    Update mid_selectQuestions.php

--- Mid/mid_selectQuestions.php
+++ Mid/mid_selectQuestions.php
@@ -4,9 +4,9 @@
     $contents = file_get_contents('php://input');
     
     $db = curl_init();
-    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/backendController.php");
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/GetBank.php");
     curl_setopt($db, CURLOPT_POST, 1);
-    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents&cmd=getBank");   
+    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents");   
     curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
     $dbexec = curl_exec($db); 
     curl_close($db);         
