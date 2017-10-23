83cc3e756505b63c4704106c64147884a5aa25a9
awdeeyo <kwevinle@gmail.com>
2017-10-23 13:50:10 -0400

    Update mid_createExam.php

--- Mid/mid_createExam.php
+++ Mid/mid_createExam.php
@@ -2,11 +2,11 @@
 <?php
     
     $contents = file_get_contents('php://input');
- 
+    
     $db = curl_init();
-    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/CreateExam.php");
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/backendController.php");
     curl_setopt($db, CURLOPT_POST, 1);
-    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents");   
+    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents&cmd=createExam");   
     curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
     $dbexec = curl_exec($db); 
     curl_close($db);  
