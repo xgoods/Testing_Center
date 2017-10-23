b4333dd1e619b1eba9363e103f552db073d276d2
awdeeyo <kwevinle@gmail.com>
2017-10-22 21:23:23 -0400

    Update grade.php

--- Mid/grade.php
+++ Mid/grade.php
@@ -5,42 +5,30 @@
     
     //get array of rules from db
     $db = curl_init();
-    curl_setopt($db, CURLOPT_URL, "rule database");
-    curl_setopt($db, CURLOPT_POST, 1);
-    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents");   
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~kl297/test/testicurl.php");  
     curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
     $dbexec = curl_exec($db); 
     curl_close($db);
     
+    $rules = json_decode($dbexec);
+    $first = $rules->{'arg1'}; 
+    $second = $rules->{'arg2'};
+    $third = $rules->{'arg3'};
+    $fourth = $rules->{'arg4'};
+    
     //execute java grader
-    $grade = exec("java grade '$studentCode' '$dbexec'");
+    $grade = exec("java grade '$studentCode' '$first' '$second' '$third' '$fourth'");
     
     //send to backend
-    $ch = curl_init();
+    /*$ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "grade database");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, "grade=$grade");   
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $gradecurl = curl_exec($ch); 
-    curl_close($ch);
+    curl_close($ch);*/
     
-    //echo $grade;
-    //echo "\n";
-
-?>
-
-
-<?php
-
-$db = curl_init();
-curl_setopt($db, CURLOPT_URL, "testicurl.php");  
-curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
-$dbexec = curl_exec($db); 
-curl_close($db);
-
-echo $dbexec;
-//$obj = json_decode($json);
-//echo $obj->{'a'}; // 12345
-echo "\n";
+    echo $grade;
+    echo "\n";
 
 ?>
