8c9beadcce52324de3707fe624d962314147e8a2
awdeeyo <kwevinle@gmail.com>
2017-10-23 17:31:13 -0400

    Create addQuestionSend.php

---
+++ random/addQuestionSend.php
@@ -0,0 +1,35 @@
+<?php
+
+  	$functionName = $_POST['functionName'];
+    $args = $_POST['args'];
+    $return = $_POST['return'];
+    $answer = $_POST['answer'];
+      
+    $question = "Write a function named " . $functionName . " that takes " . $args . " argument(s) and " . $return . " (ex.. 'returns the sum')";
+      
+    if($question == null || $answer == null){
+        echo "Please enter all the inputs.";
+        echo "<form name='backaddQuestion' action='addQuestion.php'>
+            <input type='submit' name='submit' id='submit' value='Back'>
+          </form>";
+          exit();
+    }
+      
+  $post = 'functionName='.$functionName.'&args='.$args."&returns=".$return.'&question='.$question;
+      
+	$ch = curl_init();
+ 	curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~kl297/mid_addQuestion.php");
+ 	curl_setopt($ch, CURLOPT_POST, 1);
+ 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+ 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+ 	$questionSend = curl_exec($ch);
+ 	curl_close($ch);
+  	
+  	//echo $questionSend
+  	echo "Your question has been added.";
+  	
+  	echo "<form name='backaddQuestion' action='addQuestion.php'>
+            <input type='submit' name='submit' id='submit' value='Add another question'>
+          </form>";
+	//header("Location:https://web.njit.edu/~bg245/addQuestion.php");
+?>
