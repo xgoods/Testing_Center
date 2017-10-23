3f520c461fc1db4f3598accbcdafa6c66a3bfeda
ad379 <ashd65@gmail.com>
2017-10-23 19:28:17 -0400

    Update CreateExam.php

--- CreateExam.php
+++ CreateExam.php
@@ -1,10 +1,17 @@
 <?php
-class CreateExam {
-	public function post($data,$db) {
+//class Login {
+//	public function post($data,$db) {
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
 		$eid = mysqli_query($db, "SELECT COUNT(*) FROM Exams;");
 		$eid = mysqli_fetch_array($eid);
 		$neweid = $eid[0];
-		$name = mysqli_real_escape_string($db,$data['name']);
+		$name = $_POST['examName'];
 		$result = mysqli_query($db, "INSERT INTO Exams(eid, name) VALUES('$neweid','$name');");
 		if (!$result) {
 			die(json_encode(array(
@@ -24,6 +31,6 @@ class CreateExam {
 		die(json_encode(array(
 			"status" => 1,
 			"message" => "Exam successfully created")));
-	}
-}
+//	}
+//}
 ?>
