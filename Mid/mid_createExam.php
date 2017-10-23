9b7af3291f4aa34a5681d4641db097d43f47c4a1
awdeeyo <kwevinle@gmail.com>
2017-10-23 11:31:19 -0400

    Update mid_createExam.php

--- Mid/mid_createExam.php
+++ Mid/mid_createExam.php
@@ -1,8 +1,8 @@
+//connects to brians 'selectQuestions.php'
 <?php
     
     $contents = file_get_contents('php://input');
-    
-    //connects to brians 'selectQuestions.php'
+ 
     $db = curl_init();
     curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/CreateExam.php");
     curl_setopt($db, CURLOPT_POST, 1);
