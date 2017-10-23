5a4ec591a28e728723469e6de1a1e0b6d1abf678
ad379 <ashd65@gmail.com>
2017-10-23 19:06:53 -0400

    Update GetBank.php

--- GetBank.php
+++ GetBank.php
@@ -22,7 +22,7 @@ if (mysqli_connect_errno()) {
         $return = array();
         while ($row = mysqli_fetch_assoc($result)) {
 //            $return[$row['qid']] = array($row['qid'] => $row);
-              $return[] = $row;
+              $return[] = $row['question'];
         }
         $return['status'] = 1;
         mysqli_close($db);
