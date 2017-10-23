c97cec6d46c580483c474f33c6a31a48cd892333
ad379 <ashd65@gmail.com>
2017-10-23 17:37:50 -0400

    Update CreateQuestion.php

--- CreateQuestion.php
+++ CreateQuestion.php
@@ -8,22 +8,17 @@ if (mysqli_connect_errno()) {
 		"status" => -1,
 		"message" => mysqli_connect_error())));
 }
-
-
-
 		$qid = mysqli_query($db, "SELECT COUNT(*) FROM Bank;");
 		$qid = mysqli_fetch_array($qid);
 		$question = $_POST['question'];
 		$type = "open";
 		$newqid = $qid[0];
 		$fname = $_POST['functionName'];
-		$arg1 = $_POST['args1'];
-		$arg2 = $_POST['args2'];
-		$arg3 = $_POST['args3'];
-    $answer = $_POST['answer'];
+		$args = $_POST['args'];
+    $return = $_POST['returns'];
     $ranswer = "place hold";//$_POST['ranswer'];
 
-		$result = mysqli_query($db, "INSERT INTO Bank VALUES ('$newqid','$question','$type','$fname','$arg1','$arg2','$arg3','answer','ranswer');");
+		$result = mysqli_query($db, "INSERT INTO Bank VALUES ('$newqid','$question','$type','$fname','$args','$return','$ranswer');");
 		if (!$result) {
 			$status = -1;
 			$message = mysqli_error($db);
