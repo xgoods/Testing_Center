c485d35aea47f70a4adb85dafe04d7670108d84d
awdeeyo <kwevinle@gmail.com>
2017-10-24 13:21:16 -0400

    Update print.php

--- Mid/print.php
+++ Mid/print.php
@@ -173,6 +173,23 @@
 
 ?>
 
+//mid_ListStudentGrades.php--------------------------------------
+
+<?php
+    
+    $contents = file_get_contents('php://input');
+    
+    $db = curl_init();
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/ListStudentGrades.php");
+    curl_setopt($db, CURLOPT_POST, 1);
+    curl_setopt($db, CURLOPT_POSTFIELDS, "$contents");   
+    curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
+    $dbexec = curl_exec($db); 
+    curl_close($db);  
+    
+    echo $dbexec;
+    
+?>
 
 //grade.java --------------------------------------
 
