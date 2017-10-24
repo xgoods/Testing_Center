4977b643add6811136a36ef6bcf86cae40e5eac3
ad379 <ashd65@gmail.com>
2017-10-24 02:30:46 -0400

    Update SetStudentGrade.php

--- SetStudentGrade.php
+++ SetStudentGrade.php
@@ -1,12 +1,19 @@
 <?php
-class SetStudentGrade {
-	public function post($data,$db) {
-		$eid = $data['eid'];
-		$uid = $data['uid'];
-		$grade = mysqli_query($db,"SELECT SUM(Answers.qgrade) WHERE Answers.eid = $eid AND Answers.uid = $uid;");
-		$result = mysqli_query($db,"INSERT INTO Grades VALUES ('$eid','$grade','$uid');");
+//class Login {
+//	public function post($data,$db) {
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
+		$eid = $_POST['eid'];
+		$uid = $_POST['uid'];
+   $grade = $_POST['grade'];
+		$result = mysqli_query($db,"INSERT INTO Grades(uid, eid, grade) VALUES ('$uid','$eid','$grade');");
 		die(json_encode(array(
 			"status" => 1)));
-	}
-}
+//	}
+//}
 ?>
