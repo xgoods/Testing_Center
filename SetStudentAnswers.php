11b3c3865c4f838de701bf4556e5a046947f6a88
ad379 <ashd65@gmail.com>
2017-10-24 04:26:10 -0400

    Works on backend.

--- SetStudentAnswers.php
+++ SetStudentAnswers.php
@@ -1,12 +1,15 @@
 <?php
-class SetStudentAnswers {
-	public function post($data,$db) {
-		unset($data['cmd']);
-		$data=$data['data'];
+//class Login {
+//	public function post($data,$db) {
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
 		$eid = $data['eid'];
-		unset($data['eid']);
 		$uid = $data['uid'];
-		unset($data['uid']);
     $answer = $data['answer'];
     $qgrade = $data['grade'];
 		$result = mysqli_query($db,"SELECT * FROM Grades WHERE uid = '$uid' AND eid = '$eid'");
@@ -20,6 +23,6 @@ class SetStudentAnswers {
 		die(json_encode(array(
 			"status" => 1)));
    }
-	}
-}
+//	}
+//}
 ?>
