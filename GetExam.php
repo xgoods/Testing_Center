f86b3c30b9937c8a7ad7ac15d99047890e89e0df
ad379 <ashd65@gmail.com>
2017-10-24 01:26:16 -0400

    Tested on back end: works

--- GetExam.php
+++ GetExam.php
@@ -1,17 +1,28 @@
 <?php
-class GetExam {
-	public function post($data,$db) {
-		$eid = $data['data'];
+//class Login {
+//	public function post($data,$db) {
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
+    
+		$eid = $_POST['eid'];
 		$return = array();
-		$exams = mysqli_query($db, "SELECT * FROM QAA WHERE eid = '$eid';");
+		$exams = mysqli_query($db, "SELECT Bank.question FROM ((SELECT * FROM QAA WHERE (QAA.eid=$eid)) AS T1) INNER JOIN (Bank) ON (Bank.qid = T1.qid);");
 		if (!$exams) {
 			die(json_encode(array(
 				"status" => -1,
 				"message" => mysqli_error($db))));
 		}
-		while($exam = mysqli_fetch_array($exams)) {
-			$return[$exam['qid']] = $exam['qid'];
-		}
-		die(json_encode($return));
-	}
-}
+        while ($row = mysqli_fetch_assoc($exams)) {
+//            $return[] = array($row['qid'] => $row['question']);
+            $return[] = $row['question'];
+        }
+		$je = (json_encode($return));
+    echo $je;
+//	}
+//}
+?>
