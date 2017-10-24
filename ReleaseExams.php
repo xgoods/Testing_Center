cba64f932883cf37a858c42f0fbbf9ca90753d8f
ad379 <ashd65@gmail.com>
2017-10-24 04:25:23 -0400

    Not tested at all

--- ReleaseExams.php
+++ ReleaseExams.php
@@ -1,40 +1,25 @@
 <?php
-    class ReleaseExams {
-        public function post($db, $data) {
-            $eid = $data['eid'];
-            $result = mysqli_query($db,
-                "SELECT User.uid, Grades.eid, Grades.grade FROM User LEFT OUTER JOIN Grades
-                 ON User.uid = Grades.uid && Grades.eid = $eid
-                 WHERE User.role = 'student';");
-            if (!$result) {
-                mysqli_close($db);
-                die(json_encode(array(
-                    "status" => -1,
-                    "message" => "query failed")));
-            }
-			
-			$release = mysqli_query($db,"UPDATE Exams SET released = 'r' WHERE eid = $eid;");
+//class Login {
+//	public function post($_POST,$db) {
+$db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
+if (mysqli_connect_errno()) {
+	http_response_code(500);
+	die(json_encode(array(
+		"status" => -1,
+		"message" => mysqli_connect_error())));
+}
+      $eid = $_POST['eid'];
+			$release = mysqli_query($db,"UPDATE Grades SET release = 'r' WHERE eid = $eid;");
 			if (!$release) {
                 mysqli_close($db);
                 die(json_encode(array(
                     "status" => -1,
                     "message" => "query failed")));
             }
-            $return = array();
-            while ($row = mysqli_fetch_array($result)) {
-                if ($row['grade'] == null) {
-                    $uid = $row['uid'];
-                    $insert = mysqli_query($db,
-                        "INSERT INTO Grades(uid, eid, grade) VALUES( ('$uid'), $eid, 0);");
-                    $return[$row['uid']] = array($row['uid'] => $row);
-                }
-                else {
-                    $return[$row['uid']] = array($row['uid'] => $row);
-                }
-            }
+
             $return['status'] = 1;
             mysqli_close($db);
             die(json_encode($return));
-        }
-    }
+//        }
+//    }
 ?>
