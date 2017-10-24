5798e322af26767c5ae68782e0aa41cd6db6d511
awdeeyo <kwevinle@gmail.com>
2017-10-24 12:56:39 -0400

    Update print.php

--- Mid/print.php
+++ Mid/print.php
@@ -146,7 +146,7 @@
     $contents = file_get_contents('php://input');
     
     $db = curl_init();
-    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/backendController.php");
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~ad379/ReleaseExams.php");
     curl_setopt($db, CURLOPT_POST, 1);
     curl_setopt($db, CURLOPT_POSTFIELDS, "$contents");   
     curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
