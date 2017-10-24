284804aa103515e246f32b8d3da916bf02fcf616
ad379 <ashd65@gmail.com>
2017-10-24 00:48:49 -0400

    Update CreateExam.php

--- CreateExam.php
+++ CreateExam.php
@@ -8,12 +8,13 @@ if (mysqli_connect_errno()) {
 		"status" => -1,
 		"message" => mysqli_connect_error())));
 }
+//    $contents = file_get_contents('php://input');
 		$eid = mysqli_query($db, "SELECT COUNT(*) FROM Exams;");
 		$eid = mysqli_fetch_array($eid);
 		$neweid = $eid[0];
 		$name = $_POST['examName'];
 		$result = mysqli_query($db, "INSERT INTO Exams(eid, name) VALUES('$neweid','$name');");
-    $questionarr = $_POST['questions'];
+/*    $questionarr = explode(" ",$_CURL['questions']);
 		foreach($questionarr as $question) {
 //      $qid = mysqli_query($db, "SELECT qid FROM BANK WHERE question = $question;");
 			$result = mysqli_query($db, "INSERT INTO QAA VALUES('$neweid','$question');");
@@ -23,9 +24,10 @@ if (mysqli_connect_errno()) {
 					"message" => mysqli_error($db))));
 			}
 		}
-		die(json_encode(array(
-			"status" => 1,
-			"message" => "Exam successfully created")));
+*/
+//		die(json_encode(array(
+//			"status" => 1,
+//			"message" => "Exam successfully created")));
 //	}
 //}
 ?>
