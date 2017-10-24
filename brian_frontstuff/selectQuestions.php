5491b0098f8fa08b4523ec17442e0efe4356c439
xgoods <goods.brian@gmail.com>
2017-10-23 23:28:41 -0400

    changed selectQuestions.php

--- brian_frontstuff/selectQuestions.php
+++ brian_frontstuff/selectQuestions.php
@@ -7,7 +7,7 @@
 
 <?php
 	$post = "questionBank";
-	$url = 'kevins get question curl';
+	$url = 'https://web.njit.edu/~ad379/GetBank.php';
 	
 	$ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -22,43 +22,35 @@
     echo "<form name='createExam' method='post' action='selectQuestions.php' value=></br>";
     echo "Enter the exam name: <input type='text' name='examName' value='$examName'><br>";
     foreach ($questionsArray as $question){
-        $value = array_search($question, $questionsArray);
-        echo "<input type='checkbox' name='$count' id='$count' value='$value'>".$question."<br>";
+    	$value = array_search($question, $questionsArray);
+    	echo $value;
+        echo "<input type='checkbox' name='quests[]' id='quests[]' value='$value'>".$question."<br>";
         $count++;
+        
     }
+    //echo $count;
     echo "<input type='submit' name='submit' id='submit' value='Submit'>";
     echo "</form>";
     echo "<form name='backLogin' action='teacherHome.php'>
             <input type='submit' name='submit' id='submit' value='back to menu'>
           </form>";
           
-    $questionCount = array();
-    for($i = 1; $i < $count+1; $i++)
-        if(!empty($_POST[$i])) {
-            if($questionCount == null)
-                $questionCount = array($i);
-            else
-                array_push($questionCount, $i);
-        }
-
-    if($questionCount == null ) {
-        echo "You need to select atleast 1 question";
-        exit();
-    }
-    else if(empty($_POST['examName'])) {
+    
+    if (isset($_POST['quests'])) {
+    	$quests = implode(' ', $_POST['quests']);
+    	}
+   
+    //echo $quests;
+    
+   if(empty($_POST['examName'])) {
         echo "You must enter the name of the exam";
         exit();
     }
-
-    $post = null;
-    foreach($questionCount as $question)
-        if($post == null)
-            $post = $question."=".$_POST[$question];
-        else
-            $post = $post."&".$question."=".$_POST[$question];
-
-    $post = $post."&count=".$count."&examName=".$_POST['examName'];
-	$url = "kevins mid for this";
+    
+    $examName = $_POST['examName'];
+	$post = array('questions'=>$quests, 'examName'=>$examName);
+	print_r($post); 
+	$url = "https://web.njit.edu/~kl297/mid_createExam.php";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
