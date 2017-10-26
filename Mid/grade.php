df0cf3adfec20c0f51c30517bea22a99ac92c714
awdeeyo <kwevinle@gmail.com>
2017-10-26 18:35:34 -0400

    Update grade.php

--- Mid/grade.php
+++ Mid/grade.php
@@ -1,11 +1,12 @@
 <?php
     
-    $grade = 0;
+    $grade = 0; 
+    chmod("test.py", 0666);
     //assign student input to array
-    $data = $_POST['array'];
+    //$data = $_POST['array'];
+    $data = "student1 0 test1 test1 c c";
     $temparr = explode(" ", $data);
     $arr = array($temparr[2],$temparr[3],$temparr[4],$temparr[5]);
-    
     
     //get array of rules from db
     $db = curl_init();
@@ -20,21 +21,22 @@
     $third = $rules->{'2'};
     $fourth = $rules->{'3'};
     
-    exec("javac grade.java");
     while (list($key, $studentCode) = each($arr)) {
-        //execute java grader
-        $temp = exec("java grade '$studentCode' '$first' '$second' '$third' '$fourth'");
+        //check for correct function name
+        $temp = `python grade.py $studentCode $first $second $third $fourth`;
         $grade = $grade + $temp;
     }
+
+    
     
     //send to backend
-    $ch = curl_init();
+   /* $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~ad379/SetStudentGrade.php");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, "uid=$temparr[0]&eid=$temparr[1]&grade=$grade");   
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $gradecurl = curl_exec($ch); 
-    curl_close($ch);
-    
-    echo "$grade\n"; 
+    curl_close($ch);*/
+   
+    echo "$grade\n";
 ?>
