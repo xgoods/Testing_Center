57568a1f7d0a575f365f030aa983b5f0a14be9a6
awdeeyo <kwevinle@gmail.com>
2017-10-26 20:46:24 -0400

    Update grade.php

--- Mid/grade.php
+++ Mid/grade.php
@@ -1,12 +1,13 @@
 <?php
     
     $grade = 0; 
-    chmod("test.py", 0666);
     //assign student input to array
     //$data = $_POST['array'];
     $data = "student1 0 test1 test1 c c";
     $temparr = explode(" ", $data);
     $arr = array($temparr[2],$temparr[3],$temparr[4],$temparr[5]);
+    
+    $sampleinput = 'print("5")';
     
     //get array of rules from db
     $db = curl_init();
@@ -16,18 +17,32 @@
     curl_close($db);
     
     $rules = json_decode($dbexec);
-    $first = $rules->{'0'}; 
-    $second = $rules->{'1'};
-    $third = $rules->{'2'};
-    $fourth = $rules->{'3'};
+    $first = "def {$rules->{'0'}}";
+    $second = "def {$rules->{'1'}}";
+    $third = "def {$rules->{'2'}}";
+    $fourth = "def {$rules->{'3'}}";
     
     while (list($key, $studentCode) = each($arr)) {
         //check for correct function name
         $temp = `python grade.py $studentCode $first $second $third $fourth`;
         $grade = $grade + $temp;
     }
-
     
+    //write student's code to 'test.py'
+    file_put_contents ('test.py', $sampleinput);
+
+    //check for successful execution/return value
+    if(`python test.py` == null){
+        //do nothing
+    } else if(`python test.py` == 5){
+        $grade += 10;
+    }else{
+        $grade += 5;
+    }
+    
+    if($grade > 25){
+        $grade = 25;
+    }
     
     //send to backend
    /* $ch = curl_init();
@@ -39,4 +54,5 @@
     curl_close($ch);*/
    
     echo "$grade\n";
-?>
+
+?> 
