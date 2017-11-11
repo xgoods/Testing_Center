fde5c4f38d4fdcc3d7d1dd472a857368ad6739ab
awdeeyo <kwevinle@gmail.com>
2017-11-11 08:35:17 -0500

    Update getloop.php

--- Mid/test/getloop.php
+++ Mid/test/getloop.php
@@ -1,6 +1,15 @@
 <?php
     
-    $first = '';
+    $first = 'def cocks(op,a,b):
+    if op == "+":
+        return a + b
+    
+    else:
+        return -1
+    
+breast = cocks("-", 3, 3)
+    
+print(breast)';
 
         $directions = "if";
 
@@ -20,20 +29,23 @@
         }
 
         if($keyword !== "if"){
+            
             for($i = 0;$i < sizeof($forcheck);$i++){
-                if(preg_match("/^(\w+)/",$forcheck[$i])){
-                $forcheck[0] = $forcheck[$i];
-                break;
+                if(preg_match("    ",$forcheck[$i])){
+                    if(!preg_match("     ",$forcheck[$i])){
+                        $forcheck[0] = $forcheck[$i];
+                        break;
+                    }
                 }
             } 
             if (preg_match('/[A-Za-z0-9]+/',$forcheck[0])) {
                  $step = explode("$forcheck[0]", $step[1]);
-                 $result = "$keyword$step[0]";
+                 $result = "    $keyword$step[0]";
             } else{
-                 $result = "$keyword$step[1]";
+                 $result = "    $keyword$step[1]";
             }
         } else{
-            $result = "if$step[0]-1";
+            $result = "    if$step[0]-1";
         }
      echo "$result\n";    
 ?>
