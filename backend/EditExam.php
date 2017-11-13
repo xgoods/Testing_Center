d15cfb500c2d1cd2826f7a4d22e47ceadcb61eaf
ad379 <ashd65@gmail.com>
2017-11-13 13:31:28 -0500

    Update EditExam.php

--- backend/EditExam.php
+++ backend/EditExam.php
@@ -9,12 +9,18 @@ if (mysqli_connect_errno()) {
 		"message" => mysqli_connect_error())));
 }
 //    $contents = file_get_contents('php://input');
-		
+		$qid= 555555555555555555555555555;
 		$eid = $_POST['eid'];
-    $questionarr = explode(" ",$_POST['question']);
-    for ($i = 0; $i < 4; $i++) {
+    $questionarr = explode(",",$_POST['question']);
+    $c = count($questionarr);
+    for ($i = 0; $i < $c; $i++) {
       $question = $questionarr[$i];
-      $qid = mysqli_query($db, "SELECT qid FROM BANK WHERE question = $question;");
+      $q = mysqli_query($db, "SELECT qid FROM Bank WHERE (question = '$question');");
+		  while($row = mysqli_fetch_assoc($q))
+	  	  $qid = $row['qid'];
+           echo $qid;
+           echo $question;
+           echo $eid;
 			$result = mysqli_query($db, "DELETE FROM QAA WHERE(eid='$eid' AND qid='$qid');");
     }
 		die(json_encode(array(
