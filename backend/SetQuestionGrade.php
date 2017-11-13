07b90a6cd6ffb75eec77b9c01f57629b89c4404e
ad379 <ashd65@gmail.com>
2017-11-13 14:03:19 -0500

    Update SetQuestionGrade.php

--- backend/SetQuestionGrade.php
+++ backend/SetQuestionGrade.php
@@ -1,5 +1,5 @@
 <?php
-//class CreateQuestion {
+//class Login {
 //	public function post($data,$db) {
 $db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
 if (mysqli_connect_errno()) {
@@ -8,26 +8,21 @@ if (mysqli_connect_errno()) {
 		"status" => -1,
 		"message" => mysqli_connect_error())));
 }
-		$qid = $_POST['qid'];
-		$uid = $_POST['uid'];
-		$eid = $_POST['eid'];
-		$points = $_POST['points'];
-		$comments = $_POST['comments'];
-
-		$result = mysqli_query($db, "INSERT INTO Students (uid,eid,qid,points,comments) VALUES ('$uid','$eid','$qid','$points','$comments');");
-		if (!$result) {
-			$status = -1;
-			$message = mysqli_error($db);
-		}
-		else {
-			$status = 1;
-			$message = "Question successfully graded";
-		}
-		mysqli_close($db);
-		$je = (json_encode(array(
-			"status" => $status,
-			"message" => $message)));
-    echo $je;
+		$eid = $data['eid'];
+		$uid = $data['uid'];
+    $answer = $data['answer'];
+    $qgrade = $data['grade'];
+		$result = mysqli_query($db,"SELECT * FROM Grades WHERE uid = '$uid' AND eid = '$eid'");
+		$answers = mysqli_fetch_array($result);
+		if ($answers != NULL) {
+			die(json_encode(array(
+				"status" => 0,
+				"message" => "student already took this exam")));
+    } else {
+			$result = mysqli_query($db,"INSERT INTO Answers VALUES ('$eid','$qid','$uid','$answer','$qgrade');");
+		die(json_encode(array(
+			"status" => 1)));
+   }
 //	}
 //}
 ?>
