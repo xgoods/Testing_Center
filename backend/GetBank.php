335047fcbf89a7fcb146ac1b034530d6a8e2f79b
ad379 <ashd65@gmail.com>
2017-11-14 01:28:25 -0500

    Update GetBank.php

--- backend/GetBank.php
+++ backend/GetBank.php
@@ -1,6 +1,4 @@
 <?php
-//class Login {
-//	public function post($data,$db) {
 $db=mysqli_connect("sql2.njit.edu","ad379","admin","ad379");
 if (mysqli_connect_errno()) {
 	http_response_code(500);
@@ -19,12 +17,8 @@ if (mysqli_connect_errno()) {
         $return = array();
         while ($row = mysqli_fetch_assoc($result)) {
             $return[] = $row;
-//            $return[] = $row['question'];
         }
-//        $return['status'] = 1;
         mysqli_close($db);
         $je = (json_encode($return));
         echo $je;
-//    }
-//}
 ?>
