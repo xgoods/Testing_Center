b768297308ed1ab4d7a39b6db08ef6ac10fe07b5
awdeeyo <kwevinle@gmail.com>
2017-11-13 21:44:12 -0500

    Create brians.php

---
+++ Mid/test/brians.php
@@ -0,0 +1,18 @@
+<?php
+
+    $db = curl_init();
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~kl297/test.txt");  
+    curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
+    $dbexec = curl_exec($db); 
+    curl_close($db); 
+    
+    //replace $briansarray, its the array with uid,eid,and student answers;
+    $briansarray = array("student1","0","if","while","if","for");
+    $briansarray = implode('~',$briansarray);
+    
+    file_put_contents('test.php',$dbexec);
+    $output = shell_exec("php test.php $briansarray");
+
+  //  echo "$output\n";
+
+?>
