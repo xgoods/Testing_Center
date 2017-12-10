a2269f95812f8c8fc531b7419bfa3fc31fc09e73
xgoods <goods.brian@gmail.com>
2017-12-10 14:24:22 -0500

    changed

---
+++ brian_frontstuff/sendTakeExam.php
@@ -0,0 +1,49 @@
+<html>
+<link rel="stylesheet" href="css/style.css">
+</html>
+<?php
+    $examArray = array();
+    array_push($examArray, $_POST['userName']);
+   	array_push($examArray, $_POST['examName']);
+   	
+   	$an = array_merge($examArray, $_POST['count']);
+    //echo "Exam Submitted.";
+   	$finalArray = implode('~',$an);
+    $finalArray=urlencode($finalArray);
+   	print_r($finalArray);
+    $db = curl_init();
+    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~kl297/test.txt");  
+    curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
+    $dbexec = curl_exec($db); 
+    curl_close($db); 
+    
+    //replace $briansarray, its the array with uid,eid,and student answers;
+    $briansarray = array("student1","0","if","while","if","for");
+    $briansarray = implode('~',$briansarray);
+    
+    file_put_contents('test.php',$dbexec);
+    $output = shell_exec("php test.php '$finalArray'");
+  	//  echo "$output\n";
+
+   	
+    
+   	
+   	$url2 = "https://web.njit.edu/~kl297/grade.php";
+   	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url2);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "array=$finalArray");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $hello = curl_exec($ch);
+    curl_close($ch);
+    
+    //echo "<div class='login-card'>";
+    
+    echo "<h2 style='color:white'>Exam Submitted.</h2>";
+    echo "<form name='backLogin' action='studentHome.php'>
+            <input class = 'teacherBtn' type='submit' name='submit' id='submit' value='Back to Menu'>
+          </form>"
+    //echo "</div>";
+    
+   
+?>
--- brian_frontstuff/takeExam.php
+++ brian_frontstuff/takeExam.php
@@ -18,7 +18,7 @@
 	
 	$examName = $_POST['examSelect'];
 	$userName = $_SESSION['user'];
-	echo $examName;
+	
 	$url = "https://web.njit.edu/~kl297/mid_getExam.php";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -32,13 +32,13 @@
     echo "<div class='question-card'>";
     echo "<class='logo' float='left'><img src='img/logo.jpg'>";
     //echo "<h2 align='center'>$examName</h2>";
-    echo "<form name='takeExam' id='takeExam' method='post' action='takeExam.php'></br>";
+    echo "<form name='takeExam' id='takeExam' method='post' action='sendTakeExam.php'></br>";
    	
     //$examName = $_POST['examSelect'];
     foreach($questionArray as $question) {
     echo "<p>".$question['question']."</p>";
     
-    echo "Answer: <textarea name='count[]' rows=20 cols=80></textarea>";
+    echo "Answer: <textarea type='text' name='count[]' rows=20 cols=80></textarea>";
 	}
    	//print_r($examName);
    	
@@ -47,38 +47,13 @@
    	echo "<p>";
    	echo "<input class='teacherBtn' type='submit' name='submit' value='Submit'>";
    	echo "</p>";
-   	array_push($examArray, $_POST['userName']);
-   	array_push($examArray, $_POST['examName']);
-   	
-   	$an = array_merge($examArray, $_POST['count']);
+   
     
     echo "</form>";
-   	$finalArray = implode('~',$an);
-   	print_r($finalArray);
-    $db = curl_init();
-    curl_setopt($db, CURLOPT_URL, "https://web.njit.edu/~kl297/test.txt");  
-    curl_setopt($db, CURLOPT_RETURNTRANSFER, 1);
-    $dbexec = curl_exec($db); 
-    curl_close($db); 
+     echo "<form name='backLogin' action='studentHome.php'>
+            <input class = 'teacherBtn' type='submit' name='submit' id='submit' value='Back to Menu'>
+          </form>";
     
-    //replace $briansarray, its the array with uid,eid,and student answers;
-    $briansarray = array("student1","0","if","while","if","for");
-    $briansarray = implode('~',$briansarray);
-    
-    file_put_contents('test.php',$dbexec);
-    $output = shell_exec("php test.php $finalArray");
-  	//  echo "$output\n";
-
-   	
-   	//print_r($finalArray);
-   	
-   	$url2 = "https://web.njit.edu/~kl297/grade.php";
-   	$ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url2);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "array=$finalArray");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $hello = curl_exec($ch);
-    curl_close($ch);
+    echo "</div>";
 
 ?>
